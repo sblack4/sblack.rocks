306fa862926084cb5d2cfaab42b741fcdaa69dcf
sblack4 <genseb7@gmail.com>
2018-04-24 15:57:17 -0400

    changed origin

--- deploys.sh
+++ deploys.sh
@@ -4,11 +4,10 @@ YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
-git config --global password "${GITHUB_TOKEN}"
 git config --global push.default simple
-git remote set-url origin https://github.com/sblack4/sblack4.github.io.git
+echo "${GITHUB_TOKEN}" | git remote set-url origin https://github.com/sblack4/sblack4.github.io.git
 # export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
-# git fetch --tags
+ git fetch --tags
 # msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
 # if git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
 # git tag $GIT_TAG -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
