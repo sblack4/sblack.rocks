9301563cf3beafd764ebc4bfc99577e7f3065bd0
sblack4 <genseb7@gmail.com>
2018-04-24 15:32:42 -0400

    changed push method

--- deploys.sh
+++ deploys.sh
@@ -5,7 +5,7 @@ MONTH=$(date +"%m")
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git config --global push.default simple
-# git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+git remote add origin https://${GIT_NAME}:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 # export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
 # git fetch --tags
 # msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
@@ -14,6 +14,6 @@ git config --global push.default simple
 # git push origin master && git push origin master --tags
 # ls -aR
 cd src/public
-echo in ${pwd}
+echo in $(pwd)
 git push origin master 
 echo done
